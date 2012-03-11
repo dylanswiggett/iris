a08e13044e3daeeb732bdc5282bf8ad6e75bfccc
Dylan Swiggett <dylanswiggett@gmail.com>
2012-03-10 22:58:11 -0800

    Added hello world with imports

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <GL/gl.h>
+#include <GL/glu.h>
+#include "SDL.h"
+
+using namespace std;
+
+int main(){
+  cout << "hello world\n";
+  return 0;
+}
